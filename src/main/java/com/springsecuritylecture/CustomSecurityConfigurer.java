5738e58a7aef6b57931b7a0791dba22fc868f015
bongjoonkim <bongjoonkim96@gmail.com>
2023-10-26 00:30:54 +0900

    스프링 인증 흐름, 인가 요약 1

--- src/main/java/com/springsecuritylecture/CustomSecurityConfigurer.java
+++ src/main/java/com/springsecuritylecture/CustomSecurityConfigurer.java
@@ -6,29 +6,29 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
 
     // 테스트
-    private boolean isSecure;
-
-    @Override
-    public void init(HttpSecurity builder) throws Exception {
-        super.init(builder);
-        System.out.println("init method start");
-    }
-
-    @Override
-    public void configure(HttpSecurity builder) throws Exception {
-        super.configure(builder);
-        System.out.println("configure method start");
-        if (isSecure) {
-            System.out.println("http is required");
-        } else {
-            System.out.println("http is optional");
-
-        }
-    }
-
-    public CustomSecurityConfigurer setFlag(boolean isSecure) {
-        this.isSecure = isSecure;
-        return this;
-
-    }
+//    private boolean isSecure;
+//
+//    @Override
+//    public void init(HttpSecurity builder) throws Exception {
+//        super.init(builder);
+//        System.out.println("init method start");
+//    }
+//
+//    @Override
+//    public void configure(HttpSecurity builder) throws Exception {
+//        super.configure(builder);
+//        System.out.println("configure method start");
+//        if (isSecure) {
+//            System.out.println("http is required");
+//        } else {
+//            System.out.println("http is optional");
+//
+//        }
+//    }
+//
+//    public CustomSecurityConfigurer setFlag(boolean isSecure) {
+//        this.isSecure = isSecure;
+//        return this;
+//
+//    }
 }
--- src/main/java/com/springsecuritylecture/SecurityConfig.java
+++ src/main/java/com/springsecuritylecture/SecurityConfig.java
@@ -1,34 +1,34 @@
 package com.springsecuritylecture;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        this.httpSecurity.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
-//        this.httpSecurity.formLogin(Customizer.withDefaults());
-
+    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());  // form 로그인 창이 뜸
+        http.httpBasic(Customizer.withDefaults());      // 팝업창처럼 로그인 창이 뜸
 
-//        CustomSecurityConfigurer customSecurityConfigurer = new CustomSecurityConfigurer();
-//        customSecurityConfigurer.setFlag(true);
-        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        // 커스텀한 방법
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("Custom Entry");
+            }
+        }));
         return http.build();
     };
-
-    @Bean
-    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) ->
-                authorize.anyRequest().authenticated());
-
-        http.httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
 }
