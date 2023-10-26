55fa77d33b042eb4f388bdf0932d2ea56e937600
bongjoonkim <bongjoonkim96@gmail.com>
2023-10-26 23:37:27 +0900

    스프링 시큐리티 인증 인가 흐름 요약

--- src/main/java/com/springsecuritylecture/SecurityConfig.java
+++ src/main/java/com/springsecuritylecture/SecurityConfig.java
@@ -20,15 +20,15 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
         http.formLogin(Customizer.withDefaults());  // form 로그인 창이 뜸
-        http.httpBasic(Customizer.withDefaults());      // 팝업창처럼 로그인 창이 뜸
+//        http.httpBasic(Customizer.withDefaults());      // 팝업창처럼 로그인 창이 뜸
 
         // 커스텀한 방법
-        http.exceptionHandling(exception -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
-            @Override
-            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-                System.out.println("Custom Entry");
-            }
-        }));
+//        http.exceptionHandling(exception -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
+//            @Override
+//            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//                System.out.println("Custom Entry");
+//            }
+//        }));
         return http.build();
     };
 }
